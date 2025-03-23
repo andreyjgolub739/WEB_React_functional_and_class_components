d21f74ff9d738082f5bc588f7fc4dc606d9e3a9c
andreyjgolub739 <andreyjgolub739@gmail.com>
2025-03-23 21:56:43 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserInfo name="Андрій" age="19"/>
+      <Counter />
     </div>
   );
+}
+
+// Завдання 1: Створення функціонального компоненту
+function UserInfo(props) {
+  return <h1>Привіт, мене звати {props.name},
+          мені {props.age} років</h1>;
+}
+
+// Завдання 2: Створення класового компоненту
+class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1});
+  }
+
+  render() {
+    return ( 
+    <div>
+      <h2>Лічильник. Число: {this.state.count}</h2>
+      <button onClick={this.incrementCount}>Збільшити</button>
+    </div>
+    ); 
+  }
 }
 
 export default App;
